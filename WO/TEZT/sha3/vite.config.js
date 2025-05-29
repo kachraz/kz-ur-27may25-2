0000d9eb92cb5b144067e42975f85246a0a47d8a
kachraz <110726496+kachraz@users.noreply.github.com>
2025-05-29 19:45:31 +0000

    🫦

--- WO/TEZT/sha3/vite.config.js
+++ WO/TEZT/sha3/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+import { defineConfig } from "vite"
+
+// These are required to resolve the __dirname and __filename variables in ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
